5152af3ce5f425944c3a59e698280ad0c0eeb292
Jonathan Fermin <jofe4577@colorado.edu>
2018-03-06 14:56:46 -0700

    added 5 inputs to wek/arduino

--- INon6448_ArduinoSerialToOSC/RunOnArduino/RunOnArduino.ino
+++ INon6448_ArduinoSerialToOSC/RunOnArduino/RunOnArduino.ino
@@ -6,25 +6,36 @@
 // You can add or remove pins to read from, but be sure they are separated by commas, and print a
 // newline character at the end of each loop()
 
-int AnalogPin0 = A0; //Declare an integer variable, hooked up to analog pin 0
-int AnalogPin1 = A1; //Declare an integer variable, hooked up to analog pin 1
-
 void setup() {
   Serial.begin(9600); //Begin Serial Communication with a baud rate of 9600
 }
-
+int Value1;
+int Value2;
+int Value3;
+int Value4;
+int Value5;
+  
 void loop() {
    //New variables are declared to store the readings of the respective pins
-  int Value1 = analogRead(AnalogPin0);
-  int Value2 = analogRead(AnalogPin1);
-  
+
+  Value1 = analogRead(A0);
+  Value2 = analogRead(A1);
+  Value3 = analogRead(A2);
+  Value4 = analogRead(A3);
+  Value5 = analogRead(A4);
   /*The Serial.print() function does not execute a "return" or a space
       Also, the "," character is essential for parsing the values,
       The comma is not necessary after the last variable.*/
-  
+//  
   Serial.print(Value1, DEC); 
   Serial.print(",");
-  Serial.print(Value2, DEC); 
+  Serial.print(Value2, DEC);
+  Serial.print(",");
+  Serial.print(Value3, DEC);  
+  Serial.print(",");
+  Serial.print(Value4, DEC);
+  Serial.print(",");
+  Serial.print(Value5, DEC);
   Serial.println();
   //delay(500); // For illustration purposes only. This will slow down your program if not removed 
 }
